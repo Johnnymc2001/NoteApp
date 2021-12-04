03d6e3bc1c26cf661599b3035c41de29a10dd460
Johnnymc2001 <37579854+Johnnymc2001@users.noreply.github.com>
2021-12-04 13:53:41 +0700

    Fix Url String

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -54,8 +54,8 @@ namespace API.Extensions
 
 				// Whether the connection string came from the local development configuration file
 				// or from the environment variable from Heroku, use it to set up your DbContext.
-				// options.UseNpgsql(connStr);
-				options.UseNpgsql("Host=localhost;Database=NoteApp;Username=postgres;Password=123456789");
+				options.UseNpgsql(connStr);
+				// options.UseNpgsql("Host=localhost;Database=NoteApp;Username=postgres;Password=123456789");
 			});
 
 			services.AddScoped<IUserRepository, UserRepository>();
